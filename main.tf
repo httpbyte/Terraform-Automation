1473d55c83f9fb7f2f905926244a7a63a72a2a8b
Roosevelt Mendieta <15744719+httpbyte@users.noreply.github.com>
2026-03-29 23:04:29 -0400

    added sub association

--- main.tf
+++ main.tf
@@ -24,7 +24,6 @@ resource "aws_subnet" "myapp-subnet-1" {
     }
 }
 
-
 resource "aws_route_table" "myapp-route-table" {
     vpc_id = aws_vpc.myapp-vpc.id
     route {
@@ -41,4 +40,9 @@ resource "aws_internet_gateway" "myapp-igw" {
     tags = {
       Name: "${var.env_prefix}-igw"
     }
+}
+
+resource "aws_route_table_association" "a-rtb-subnet" {
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    route_table_id = aws_route_table.myapp-route-table.id
 }
