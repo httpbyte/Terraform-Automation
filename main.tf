1f5e3e0641e2566f0ea516014670ca5af1be8350
Roosevelt Mendieta <15744719+httpbyte@users.noreply.github.com>
2026-03-29 22:22:10 -0400

    cleanup and formatting route table for vpc

--- main.tf
+++ main.tf
@@ -3,52 +3,27 @@ provider "aws" {
     region = "us-east-1"
 }
 
-variable "subnet_cidr_block" {
-    description = "subnet cidr block"
-}
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
 
-variable "vpc_cidr_block" {
-    description = "vpc cidr block"
-}
-
-resource "aws_vpc" "development-vpc" {
+resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
       tags = {
-      Name: "development",
-      vpc_env: "dev"
+        Name: "${var.env_prefix}-vpc"
     }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-    vpc_id = aws_vpc.development-vpc.id
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id = aws_vpc.myapp-vpc.id
     cidr_block = var.subnet_cidr_block
-    availability_zone = "us-east-1a"
+    availability_zone = var.avail_zone
       tags = {
-      Name: "subnet-1-dev"
+        Name: "${var.env_prefix}-subnet-1"
     }
 }
 
-data "aws_vpc" "existing_vpc" {
-    default = true
-}
-
-resource "aws_subnet" "dev-subnet-2" {
-    vpc_id = data.aws_vpc.existing_vpc.id
-    cidr_block = "172.31.96.0/20"
-    availability_zone = "us-east-1a"
-      tags = {
-      Name: "subnet-2-default"
-    }
-}
-
-output "dev-vpc-id" {
-    value = aws_vpc.development-vpc.id
-} 
-
-output "dev-subnet-id" {
-    value = aws_subnet.dev-subnet-1.id
-}
-
-output "dev-subnet-2-id" {
-    value = aws_subnet.dev-subnet-2.id
+resource "aws_route_table" "myapp-route-table" {
+    
 }
