d5f937a352bb4bfd2008b756f01ba9529aaaf2c9
Roosevelt Mendieta <15744719+httpbyte@users.noreply.github.com>
2026-03-31 23:03:36 -0400

    added aws ami test value

--- main.tf
+++ main.tf
@@ -73,3 +73,25 @@ resource "aws_default_security_group" "default-sg" {
        Name: "${var.env_prefix}-default-sg"
     }
 }
+
+data "aws_ami" "latest-amazon-linux-image" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+output "aws_ami" {
+    value = data.aws_ami.latest-amazon-linux-image
+}
+
+# resource "aws_instance" "myapp-server" {
+#     ami = data.aws_ami.latest-amazon-linux-image.id
+
+# }
